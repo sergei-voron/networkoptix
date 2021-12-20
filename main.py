41f1e1de135621c55d77f5a3794aa53d2c4797f9
Sergei Voron <sergei.voron@volvocars.com>
2021-12-21 00:36:03 +0100

    add 2 global variables

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ import platform
 import psutil
 from sys import platform as _platform
 
+
 def host_parameters():
-    # Use a breakpoint in the code line below to debug your script.
+    global hdd_total, hdd_used
     print('Host name: ' + socket.gethostname())
     print('OS version: ' + platform.platform())
-    print('CPU cores: ' + str(os.cpu_count()))
-    print('RAM in mb: ' + str(int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')/1024.**2)))
+    print('CPU cores: ', os.cpu_count())
+    # print('RAM in mb: ' + str(int(os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / 1024. ** 2)))
+    print('RAM memory total mb:', int(psutil.virtual_memory()[0] / 1024. ** 2))
     if _platform == "linux" or platform == "linux2":
-        hdd_total = str(int(psutil.disk_usage('/').total/1024.**3))
-        hdd_used = str(int(psutil.disk_usage('/').used/1024.**3))
+        hdd_total = int(psutil.disk_usage('/').total / 1024. ** 3)
+        hdd_used = int(psutil.disk_usage('/').used / 1024. ** 3)
     elif platform == "win32":
-        hdd_total = str(int(psutil.disk_usage('C:\\').total/1024.**3))
-        hdd_used = str(int(psutil.disk_usage('C:\\').used/1024.**3))
-    print('HDD size in gb: ' + hdd_total)
-    print('HDD disk usage in gb: ' + hdd_used)
+        hdd_total = int(psutil.disk_usage('C:\\').total / 1024. ** 3)
+        hdd_used = int(psutil.disk_usage('C:\\').used / 1024. ** 3)
+    print('HDD size in gb: ', hdd_total)
+    print('HDD disk usage in gb: ', hdd_used)
 
 
 if __name__ == '__main__':
